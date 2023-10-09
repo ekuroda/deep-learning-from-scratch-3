2fd5b6e81059f97e79627f3cc8940a8bd624aefe
ekuroda <kurodaeishi@gmail.com>
2023-10-09 15:21:22 +0900

    45

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -16,7 +16,11 @@ else:
     from dezero.core import as_array
     from dezero.core import as_variable
     from dezero.core import setup_variable
+    from dezero.layers import Layer
+    from dezero.models import Model
 
     import dezero.functions
+    import dezero.layers
+    import dezero.utils
 
 setup_variable()
